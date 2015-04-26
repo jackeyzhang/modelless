9b77f0210f13783370bd55d867b133a48a207ba4
jacky <zhangchenhao@139.com>
2015-04-26 11:54:47 +0800

    first commit

---
+++ src/main/java/org/jacky/modelless/App.java
@@ -0,0 +1,13 @@
+package org.jacky.modelless;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
